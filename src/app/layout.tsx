9a501bb6ad4596fca721ba763815907b6e7951fc
Aqib-N <aqibalipk777@gmail.com>
2024-03-16 11:40:47 +0500

    fix SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,12 +5,44 @@ import "./theme-config.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Multi Speaking Studio",
+  title: {
+    default: "Multi Speaking Studio",
+    template: "%s | Multi Speaking Studio",
+  },
   description:
-    "Discover the joy of language learning at our website! Master English, Italian, French, German, and Spanish with our interactive lessons and expert guidance. Join our vibrant community for personalized feedback and cultural insights. Start your multilingual journey today! ",
+    "Discover the joy of language learning at our website! Master English, Italian, French, German, and Spanish with our interactive lessons and expert guidance. Join our vibrant community for personalized feedback and cultural insights. Start your multilingual journey today!. Multi Speaking Studio | Language Learning Hub | Speech Training Center |  Polyglot Studio | Communication Skills Institute | Linguistic Mastery Studio | Multicultural Speaking Workshop  | Diverse Language Studio | Public Speaking and Language Center | nternational Communication Studio.",
+  referrer: "origin-when-cross-origin",
+  keywords: [
+    "Multi Speaking Studio",
+    "Speaking Studio",
+    "Multi Speaking",
+    "English ",
+    "English learning",
+    "English speaking",
+    "Learn English online",
+    "ESL (English as a Second Language)",
+    "English language courses",
+    "English grammar",
+    "Vocabulary building",
+    "English speaking practice",
+    "English writing skills",
+    "English listening comprehension",
+    "English pronunciation",
+    "TOEFL preparation",
+    "IELTS preparation",
+    "Business English",
+    "Academic English",
+    "English for beginners",
+    "English for kids",
+    "English conversation",
+    "English fluency",
+    "English exams",
+    "English tutoring",
+  ],
+  authors: [{ name: "Seb", url: "https://www.multispeakingstudio.com" }],
 
   icons: {
-    icon: "/MSS.png",
+    icon: "/MSS-modified.png",
   },
 };
 
